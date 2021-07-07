1ec750eb4914fa9ddc1527eb217d7333b564fd75
augustoibarrola <augusto.t.ibarrola@gmail.com>
2021-07-07 19:01:29 -0400

    removed uneeded imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router'; /* imports RouterModule and Routes give the application routing functionality */
-import { HeroesDeckComponent } from './heroes/heroes-deck/heroes-deck.component';
-import { HeroDetailComponent } from './heroes/heroes-deck/hero-detail/hero-detail.component';
-import { DirectivesDemoComponent } from './notes/directives-demo/directives-demo.component';
-import { TemplateFormComponent } from './notes/forms/template-form/template-form.component';
-import { ReactiveFormComponent } from './notes/forms/reactive-form/reactive-form.component';
-import { PipesComponent } from './notes/pipes/pipes.component';
-import { ServicesComponent } from './notes/services/services.component';
-import { FormsComponent } from './notes/forms/forms.component';
-import { ValidationsComponent } from './notes/forms/validations/validations.component';
-import { RouterComponent } from './notes/router/router.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 
